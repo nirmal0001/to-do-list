a1b10ab88c2632a22a58f9ebe85a7f3dc907f153
nirmal0001 <kdjj18322@gmail.com>
2025-06-17 14:38:16 +0530

    init  completed html add form pending

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./reset.css"
+import "./styles.css"
+
+// Classes:
+// - Todo
+//   - properties: title, desc, date, priority[high, low, medium color based], notes, checklist[]
+//   - methods: markDone(), edit(), export()
+
+// - Project
+//   - properties: name, todos[]
+//   - methods: addTodo(), deleteTodo(), export()
+
+// Modules:
+// - Logic Module
+//   - create/edit/delete todos & projects
+
+// - UI Module
+//   - display projects, todos, and forms
+// make a commit befor js
